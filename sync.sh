aafee0f26b264137a3fb78cb0cc9f501468fd534
Michael Ennen <mike.ennen@gmail.com>
2017-08-27 16:41:03 -0700

    Add jdk9 to sync.sh.

--- sync.sh
+++ sync.sh
@@ -46,8 +46,8 @@ submit_pr() {
     tar -xf "$sha"-res.tar.gz --strip-components 3
     rm "$sha"-res.tar.gz
     remote="$(find_git_remote)"
-    printf "Opening PR to %s for %s\n" "$remote" "$version"-"$build"
-    printf "Summary of changes:\n"
+    printf "Opening PR to %s for %s\\n" "$remote" "$version"-"$build"
+    printf "Summary of changes:\\n"
     git --no-pager diff --stat
     git fetch "$remote" master
     git checkout -b "$version"-"$build" "$remote"/master
@@ -98,65 +98,72 @@ install_prereqs() {
   fi
 }
 
+fetch_highest_builds() {
+  tag_url=$1
+  start_hash=$2
+  echo "Fetching highest builds for tag url ${tag_url} with starting hash ${start_hash}"
+  raw_tags=$(curl -s "${tag_url}" | ${pup} '.tagEntry text{}' | sed "/$start_hash/q" | xargs echo -n)
+
+  read -a raw <<< "$raw_tags"
+  declare -A tags
+
+  for (( i=0; i<${#raw[@]}; i+=2 )); do
+    if [ ! "${raw[i]}" == "tip" ]; then
+      tags[${raw[i]}]=${raw[i+1]}
+    fi
+  done
+
+  printf '%s\n' "${tags[@]}"
+  declare -A highest_builds
+
+  for key in "${!tags[@]}"
+  do
+    version=$(echo "$key" | cut -f1 -d-)
+    build=$(echo "$key" | cut -f2 -d-)
+    if [[ -z "${highest_builds[${version}]}" ]]; then
+      # Have not yet seen a build for this version, so add this one
+      highest_builds[$version]=$build
+    else
+      # We have seen a build for this version, see if this one is higher
+      if [ "${build:1}" -gt "${highest_builds[$version]:1}" ]; then
+        highest_builds[$version]=$build
+      fi
+    fi
+  done
+
+  read -a monocle_tags <<<"$(git tag -l | xargs echo -n)"
+  for monocle_tag in "${monocle_tags[@]}"
+  do
+    if [[ $monocle_tag =~ "-" ]]; then
+      version=$(echo "$monocle_tag" | cut -f1 -d-)
+      build=$(echo "$monocle_tag" | cut -f2 -d-)
+      if [[ ! -z "${highest_builds[${version}]}" ]]; then
+        if [ "${highest_builds[${version}]}" == "$build" ]; then
+          # We can skip this version, as the highest build is already a tag
+          # in the upstream monocle repository.
+          printf "Version %s already has the highest build %s\\n" "$version" "$build"
+        else
+          # There is a newer build available
+          printf "The latest build for version %s is %s in upstream, but there is a newer build %s\\n" "$version" "$build" "${highest_builds[${version}]}"
+          submit_pr "$version" "${highest_builds[${version}]}" "${tags["$version"-"${highest_builds[$version]}"]}" "$hub"
+          highest_builds[${version}]=''
+        fi
+      fi
+  fi
+  done
+  for key in "${!highest_builds[@]}"
+  do
+    if [[ ! -z "${highest_builds[$key]}" ]]; then
+      submit_pr "$key" "${highest_builds[${key}]}" "${tags["$key"-"${highest_builds[$key]}"]}" "$hub"
+    fi
+  done
+}
+
 require_executable git
 require_executable curl
 require_executable wget
 require_executable unzip
 install_prereqs
 
-raw_tags=$(curl -s http://hg.openjdk.java.net/openjfx/8u-dev/rt/tags | ${pup} '.tagEntry text{}' | sed '/3a7f004c4995/q' | xargs echo -n)
-
-declare -A tags
-read -a raw <<< "$raw_tags"
-
-for (( i=0; i<${#raw[@]}; i+=2 )); do
-  if [ ! "${raw[i]}" == "tip" ]; then
-    tags[${raw[i]}]=${raw[i+1]}
-  fi
-done
-
-declare -A highest_builds
-
-for key in "${!tags[@]}"
-do
-  version=$(echo "$key" | cut -f1 -d-)
-  build=$(echo "$key" | cut -f2 -d-)
-  if [[ -z "${highest_builds[${version}]}" ]]; then
-    # Have not yet seen a build for this version, so add this one
-    highest_builds[$version]=$build
-  else
-    # We have seen a build for this version, see if this one is higher
-    if [ "${build:1}" -gt "${highest_builds[$version]:1}" ]; then
-      highest_builds[$version]=$build
-    fi
-  fi
-done
-
-read -a monocle_tags <<<"$(git tag -l | xargs echo -n)"
-for monocle_tag in "${monocle_tags[@]}"
-do
-  if [[ $monocle_tag =~ "-" ]]; then
-    version=$(echo "$monocle_tag" | cut -f1 -d-)
-    build=$(echo "$monocle_tag" | cut -f2 -d-)
-    if [[ ! -z "${highest_builds[${version}]}" ]]; then
-      if [ "${highest_builds[${version}]}" == "$build" ]; then
-        # We can skip this version, as the highest build is already a tag
-        # in the upstream monocle repository.
-        printf "Version %s already has the highest build %s\n" "$version" "$build"
-      else
-        # There is a newer build available
-        printf "The latest build for version %s is %s in upstream, but there is a newer build %s\n" "$version" "$build" "${highest_builds[${version}]}"
-        submit_pr "$version" "${highest_builds[${version}]}" "${tags["$version"-"${highest_builds[$version]}"]}" "$hub"
-        highest_builds[${version}]=''
-      fi
-    fi
-fi
-done
-
-for key in "${!highest_builds[@]}"
-do
-  if [[ ! -z "${highest_builds[$key]}" ]]; then
-    submit_pr "$key" "${highest_builds[${key}]}" "${tags["$key"-"${highest_builds[$key]}"]}" "$hub"
-  fi
-done
-
+fetch_highest_builds "http://hg.openjdk.java.net/openjfx/8u-dev/rt/tags" "3a7f004c4995"
+fetch_highest_builds "http://hg.openjdk.java.net/openjfx/9-dev/rt/tags" "dc2bda380efe"
