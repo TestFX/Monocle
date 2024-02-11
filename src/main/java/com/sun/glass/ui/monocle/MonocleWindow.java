40721f638c4acf5c74e85884bf22737af8c327ca
msoderquist <msoderquist@mastercontrol.com>
2024-02-11 11:01:20 -0700

    Update to JFX 21.0.2

--- src/main/java/com/sun/glass/ui/monocle/MonocleWindow.java
+++ src/main/java/com/sun/glass/ui/monocle/MonocleWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -266,6 +266,11 @@ final class MonocleWindow extends Window {
         }
         notifyResizeAndMove(x, y, width, height);
         return true;
+    }
+
+    @Override
+    protected void notifyMoveToAnotherScreen(Screen screen) {
+        super.notifyMoveToAnotherScreen(screen);
     }
 
     void setFullScreen(boolean fullscreen) {
